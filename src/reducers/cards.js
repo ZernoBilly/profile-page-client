4d3e343ef149fc2a0eb0271c66eec0efcef9c38e
ZernoBilly <heikki.kurppa@gmail.com>
2021-06-09 19:37:24 +0300

    Redux biolerplate code done

---
+++ src/reducers/cards.js
@@ -0,0 +1,11 @@
+export default (cards = [], action) => {
+  switch (action.type) {
+    case "FETCH_ALL":
+      return cards;
+    case "CREATE":
+      return cards;
+
+    default:
+      return cards;
+  }
+};
